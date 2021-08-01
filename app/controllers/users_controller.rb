1bbc8051191373aec23fde951742ede8f1cc32e8
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-08-01 23:20:45 +0900

    ルート追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     skip_before_action :login_required, only: [:new, :create]
 
+
+    def index
+    end
+    
     def new
         @user = User.new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'users#new'
+  
   resources :sessions, only: [:new, :create, :show, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show ]
   resources :pictures
 end
